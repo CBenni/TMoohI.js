6128e22beada40114f89a05676b643b48047b922
CBenni <levelcarnage@gmail.com>
2016-02-26 16:26:32 +0100

    Started conection handlers

--- tmoohi.js
+++ tmoohi.js
@@ -1,18 +1,19 @@
 var net = require("net");
 var _ = require("lodash");
+var jsonfile = require("jsonfile");
 var argv = require('yargs')
 	.usage('Usage: $0 <command> [options]')
-	.example('$0 --config ./some_config.json')
+	.example('$0 --config some_config.json')
 	.describe('config', 'config.json file')
-	.default('config', "./settings.json")
+	.default('config', "settings.json")
 	.help('h')
 	.alias('h', 'help')
 	.argv;
 
 var usermanager = require("./lib/usermanager");
-var messageparser = require("./lib/messageparser")
+var messageparser = require("./lib/messageparser");
+var log = require("./lib/log");
 
-var settings = require(argv.config);
 /**
 GENERAL INFO
 
@@ -25,29 +26,46 @@ user - a twitch account, identified by the twitch oauth login information (nickn
 (user) manager - manages the users that tmoohi.js is connected with
 connection - TCP connection to a TMI server
 **/
-class TMoohI {
-	constructor(settings) {
-		self.settings = settings;
-		// start the server, create the usermanager
-		self.manager = new usermanager(settings);
-		this.loadLoggers();
-		this.loadServers();
+function TMoohI(settings) {
+	var self = this;
+	self.settings = settings;
+	
+	self.loadLoggers = function() {
+		for(var i=0;i<settings.loggers.length;++i) {
+			var loggersetting = settings.loggers[i];
+			var module = require(loggersetting.module);
+			var logger = new module(loggersetting.filters, loggersetting);
+			log.addLogger(logger);
+		}
 	}
 	
-	loadServers() {
+	self.loadServers = function() {
 		var serversettings = Object.keys(settings.servers);
 		for(var i=0;i<serversettings.length;++i) {
 			var servername = serversettings[i];
+			log.write("INFO","main","Loading server "+servername);
 			var serversetting = settings.servers[servername];
-			
-			var server = new require(serversetting.module)(this, servername, serversetting);
+			var module = require(serversetting.module);
+			var server = new module(this, servername, serversetting);
 		}
 	}
 	
-	loadLoggers() {
-		for(var i=0;i<settings.loggers.length;++i) {
-			var loggersetting = settings.loggers[i];
-			var logger = new require(loggersetting.module)(loggersetting.filters, loggersetting);
+	
+	// start the server, create the usermanager
+	self.manager = new usermanager(settings);
+	self.loadLoggers();
+	log.write("INFO","main","Loaded loggers, starting servers");
+	self.loadServers();
+}
+
+if(argv.config) {
+	var settings = jsonfile.readFile(argv.config,function(err, res) {
+		if(err) {
+			console.log("Error: couldnt read settings file "+argv.config+": "+err);
+			return;
 		}
-	} 
+		var tmoohi = new TMoohI(res);
+	});
+} else {
+	console.log("Error: No config file specified.");
 }
